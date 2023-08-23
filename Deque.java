2f8d591d554e6072c927acd141edab275d306cd0
antara-walid <antara.walid.27@gmail.com>
2023-08-23 11:28:07 +0100

    initial commit

---
+++ Deque.java
@@ -0,0 +1,11 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+public class Deque<Item> implements Iterable<Item> {
+    public static void main(String[] args) {
+        // test
+    }
+}
